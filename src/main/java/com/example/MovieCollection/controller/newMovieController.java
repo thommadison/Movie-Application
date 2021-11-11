900d4ed270f1cae2b49433c6ff8d1221c618832a
dysco72 <jeffreyscp087@gmail.com>
2021-11-10 19:19:16 -0800

    Deleted redundant sources folder, added "Best film editing" movies into arraylist, optifine the RequestMapping

--- src/main/java/com/example/MovieCollection/controller/newMovieController.java
+++ src/main/java/com/example/MovieCollection/controller/newMovieController.java
@@ -8,37 +8,43 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/awardmovies")
 public class newMovieController {
 
     @Autowired
     private MovieService topicService;
 
     //return all movies
-    @RequestMapping("/awardmovies/animatedfeature/2020")
+    //@RequestMapping("/animatedfeature/2020")
+    @RequestMapping("/")
     public List<Movie> getAllMovies(){
         return topicService.getAllMovies();
     }
 
     //return individual movie
-    @RequestMapping("/awardmovies/animatedfeature/2020/{id}")
+    //@RequestMapping("/animatedfeature/2020/{id}")
+    @RequestMapping("/{id}")
     public Movie getMovie(@PathVariable String id){
         return topicService.getMovie(id);
     }
 
     //add movie
-    @RequestMapping(method = RequestMethod.POST, value ="/awardmovies/animatedfeature/2020")
+    //@RequestMapping(method = RequestMethod.POST, value ="/animatedfeature/2020")
+    @RequestMapping(method = RequestMethod.POST, value ="/")
     public void addMovie(@RequestBody Movie movie){
         topicService.addMovie(movie);
     }
 
     //update movie
-    @RequestMapping(method = RequestMethod.PUT, value ="/awardmovies/animatedfeature/2020/{id}")
+    //@RequestMapping(method = RequestMethod.PUT, value ="/animatedfeature/2020/{id}")
+    @RequestMapping(method = RequestMethod.POST, value ="/{id}")
     public void updateMovie(@RequestBody Movie movie, @PathVariable String id){
         topicService.updateMovie(id, movie);
     }
 
     //delete movie
-    @RequestMapping(method = RequestMethod.DELETE, value ="/awardmovies/animatedfeature/2020/{id}")
+    //@RequestMapping(method = RequestMethod.DELETE, value ="/animatedfeature/2020/{id}")
+    @RequestMapping(method = RequestMethod.DELETE, value ="/{id}")
     public void deleteMovie(@PathVariable String id){
         topicService.deleteMovie(id);
     }
