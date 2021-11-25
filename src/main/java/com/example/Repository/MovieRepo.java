5727ee7842f3205b918f2429c3985027f43cf240
Keochea <70778547+keochea647@users.noreply.github.com>
2021-11-25 00:21:00 -0800

    Query methods

--- src/main/java/com/example/Repository/MovieRepo.java
+++ src/main/java/com/example/Repository/MovieRepo.java
@@ -7,5 +7,9 @@ import com.example.MovieCollection.Movie;
 
 @Repository
 public interface MovieRepo extends CrudRepository<Movie,Integer> {
-
+  List<Movie> findByYearNominated(int year);
+	@Query("FROM Movie WHERE category LIKE %?1% AND yearNominated=?2 AND award_status=true")
+	List<Movie> findWinnerByYear(String award, int year);
+	@Query("FROM Movie WHERE category LIKE %?1% AND yearNominated=?2")
+	List<Movie> findNominationsByYear(String award, int year);
 }
