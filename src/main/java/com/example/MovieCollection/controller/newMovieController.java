8568050f1891eb66ae319cb2b9829c148e1190dd
Keochea <ksun8900@gmail.com>
2021-11-30 00:15:17 -0800

    Adding the rest of the search categories

--- src/main/java/com/example/MovieCollection/controller/newMovieController.java
+++ src/main/java/com/example/MovieCollection/controller/newMovieController.java
@@ -93,66 +93,250 @@ public class newMovieController {
 		modelAndView.setViewName("apiDoc");
 		return modelAndView;
 	}
+	//methods below return either the nominated movies or winners of a certain award category
 	// returns the movies of animated feature award within the input year
     @RequestMapping("/animated-feature/{year}")
-    public List<Movie> getAnimatedNominations(@PathVariable int year) {
+    public List<Movie> getAnimatedNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(ANIMATED, year);
+    }
+    @RequestMapping("/winning/animated-feature/{year}")
+    public List<Movie> getAnimatedWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(ANIMATED, year);
     }
 	// returns the movies of actor award within the input year
     @RequestMapping("/actor/{year}")
     public List<Movie> getActorNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(ACTOR, year);
     }
+    @RequestMapping("/winning/actor/{year}")
+    public List<Movie> getActorWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(ACTOR, year);
+    }
 	// returns the movies of actress award within the input year
     @RequestMapping("/actress/{year}")
     public List<Movie> getActressNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(ACTRESS, year);
+    }
+    @RequestMapping("/winning/actress/{year}")
+    public List<Movie> getActressWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(ACTRESS, year);
     }
 	// returns the movies of art direction award within the input year
     @RequestMapping("/art-direction/{year}")
     public List<Movie> getArtDirectionNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(ART_DIRECTION, year);
     }
+    @RequestMapping("/winning/art-direction/{year}")
+    public List<Movie> getArtDirectionWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(ART_DIRECTION, year);
+    }
 	// returns the movies of dance direction award within the input year
     @RequestMapping("/dance-direction/{year}")
     public List<Movie> getDanceDirectionNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(DANCE_DIRECTION, year);
+    }
+    @RequestMapping("/winning/dance-direction/{year}")
+    public List<Movie> getDanceDirectionWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(DANCE_DIRECTION, year);
     }
 	// returns the movies of cinematography award within the input year
     @RequestMapping("/cinematography/{year}")
     public List<Movie> getCinemaNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(CINEMA, year);
     }
+    @RequestMapping("/winning/cinematography/{year}")
+    public List<Movie> getCinemaWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(CINEMA, year);
+    }
 	// returns the movies of writing award within the input year
     @RequestMapping("/writing/{year}")
     public List<Movie> getWritingNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(WRITING, year);
+    }
+    @RequestMapping("/winning/writing/{year}")
+    public List<Movie> getWritingWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(WRITING, year);
     }
 	// returns the movies of directing award within thg input year
     @RequestMapping("/directing/{year}")
     public List<Movie> getDirectingNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(DIRECTING, year);
     }
+    @RequestMapping("/winning/directing/{year}")
+    public List<Movie> getDirectingWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(DIRECTING, year);
+    }
 	// returns the movies if engineering effects award within the input year
     @RequestMapping("/engineering-effects/{year}")
     public List<Movie> getEngineeringEffectsNominated(@PathVariable int year) {
     	return topicService.findNominationsByCategoryAndYear(ENGINEERING_EFFECTS, year);
     }
-	// returns the movies of costume design award within the input year
-    @RequestMapping("/costume-design/{year}")
-    public List<Movie> getCostumeNominations(@PathVariable int year) {
-    	return topicService.findNominationsByCategoryAndYear(COSTUME, year);
+    @RequestMapping("/winning/engineering-effects/{year}")
+    public List<Movie> getEngineeringEffectsWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(ENGINEERING_EFFECTS, year);
     }
-    // returns movies of film editing award within input year
+    @RequestMapping("/special-effects/{year}")
+    public List<Movie> getSpecialEffectsNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(SPECIAL_EFFECTS, year);
+    }
+    @RequestMapping("/winning/special-effects/{year}")
+    public List<Movie> getSpecialEffectsWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(SPECIAL_EFFECTS, year);
+    }
+    @RequestMapping("/visual-effects/{year}")
+    public List<Movie> getVisualEffectsNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(VISUALS, year);
+    }
+    @RequestMapping("/winning/visual-effects/{year}")
+    public List<Movie> getVisualEffectsWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(VISUALS, year);
+    }
     @RequestMapping("/film-editing/{year}")
-    public List<Movie> getFilmEditingWinner(@PathVariable int year) {
+    public List<Movie> getFilmEditingNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(FILM_EDITING, year);
+    }
+    @RequestMapping("/winning/film-editing/{year}")
+    public List<Movie> getFilmEditingWinners(@PathVariable int year) {
     	return topicService.findWinnersByCategory(FILM_EDITING, year);
     }
-	// returns movies of music award within input year
+    @RequestMapping("/best-motion-picture/{year}")
+    public List<Movie> getBestMotionPictureNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(BEST_MOTION_PICTURE, year);
+    }
+    @RequestMapping("/winning/best-motion-picture/{year}")
+    public List<Movie> getBestMotionPictureWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(BEST_MOTION_PICTURE, year);
+    }
+    @RequestMapping("/best-picture/{year}")
+    public List<Movie> getBestPictureNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(BEST_PICTURE, year);
+    }
+    @RequestMapping("/winning/best-picture/{year}")
+    public List<Movie> getBestPictureWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(BEST_PICTURE, year);
+    }
+    @RequestMapping("/outstanding-picture/{year}")
+    public List<Movie> getOutstandingPictureNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(OUTSTANDING_PICTURE, year);
+    }
+    @RequestMapping("/winning/outstanding-picture/{year}")
+    public List<Movie> getOutstandingPictureWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(OUTSTANDING_PICTURE, year);
+    }
+    @RequestMapping("/outstanding-motion-picture/{year}")
+    public List<Movie> getOutstandingMotionPictureNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(OUTSTANDING_MOTION_PICTURE, year);
+    }
+    @RequestMapping("/winning/outstanding-motion-picture/{year}")
+    public List<Movie> getOutstandingMotionPictureWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(OUTSTANDING_MOTION_PICTURE, year);
+    }
+    @RequestMapping("/outstanding-production/{year}")
+    public List<Movie> getOutstandingProductionNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(OUTSTANDING_PRODUCTION, year);
+    }
+    @RequestMapping("/winning/outstanding-production/{year}")
+    public List<Movie> getOutstandingProductionWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(OUTSTANDING_PRODUCTION, year);
+    }
+    @RequestMapping("/sound-recording/{year}")
+    public List<Movie> getSoundRecordingNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(SOUND_RECORDING, year);
+    }
+    @RequestMapping("/winning-sound-recording/{year}")
+    public List<Movie> getSoundRecordingWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(SOUND_RECORDING, year);
+    }
+    @RequestMapping("/sound-editing/{year}")
+    public List<Movie> getSoundEditingNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(SOUND_EDITING, year);
+    }
+    @RequestMapping("/winning/sound-editing/{year}")
+    public List<Movie> getSoundEditingWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(SOUND_EDITING, year);
+    }
+    @RequestMapping("/sound/{year}")
+    public List<Movie> getSoundNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(SOUND, year);
+    }
+    @RequestMapping("/winning/sound/{year}")
+    public List<Movie> getSoundWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(SOUND, year);
+    }
     @RequestMapping("/music/{year}")
-	public List<Movie> getMusicNominations(@PathVariable int year) {
+    public List<Movie> getMusicNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(MUSIC, year);
+    }
+    @RequestMapping("/winning/music/{year}")
+	public List<Movie> getMusicWinners(@PathVariable int year) {
 		return topicService.findWinnersByCategory(MUSIC, year);
 	}
+    @RequestMapping("/short-subject/{year}")
+    public List<Movie> getShortSubjectNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(SHORT_SUBJECT, year);
+    }
+    @RequestMapping("/winning/short-subject/{year}")
+    public List<Movie> getShortSubjectWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(SHORT_SUBJECT, year);
+    }
+    @RequestMapping("/short-film/{year}")
+    public List<Movie> getShortFilmNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(SHORT_FILM, year);
+    }
+    @RequestMapping("/winning/short-film/{year}")
+    public List<Movie> getShortFilmWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(SHORT_FILM, year);
+    }
+    @RequestMapping("/assistant-director/{year}")
+    public List<Movie> getAssistantDirectorNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(ASSISTANT_DIRECTOR, year);
+    }
+    @RequestMapping("/winning/assistant-director/{year}")
+    public List<Movie> getAssistantDirectorWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(ASSISTANT_DIRECTOR, year);
+    }
+    @RequestMapping("/makeup/{year}")
+    public List<Movie> getMakeupNomianted(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(MAKEUP, year);
+    }
+    @RequestMapping("/winning/makeup/{year}")
+    public List<Movie> getMakeupWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(MAKEUP, year);
+    }
+    @RequestMapping("/international-film/{year}")
+    public List<Movie> getInternationalNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(INTERNAT_FILM, year);
+    }
+    @RequestMapping("/winning/international-film/{year}")
+    public List<Movie> getInternationalWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(INTERNAT_FILM, year);
+    }
+    @RequestMapping("/foreign-film/{year}")
+    public List<Movie> getForeignNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(FOREIGN_FILM, year);
+    }
+    @RequestMapping("/winning/foreign-film/{year}")
+    public List<Movie> getForeignWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(FOREIGN_FILM, year);
+    }
+    @RequestMapping("/documentary/{year}")
+    public List<Movie> getDocumentaryNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(DOCUMENTARY, year);
+    }
+    @RequestMapping("/winning/documentary/{year}")
+    public List<Movie> getDocumentaryWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(DOCUMENTARY, year);
+    }
+	// returns the movies of costume design award within the input year
+    @RequestMapping("/costume-design/{year}")
+    public List<Movie> getCostumeNominated(@PathVariable int year) {
+    	return topicService.findNominationsByCategoryAndYear(COSTUME, year);
+    }
+    @RequestMapping("/winning/costume-design/{year}")
+    public List<Movie> getCostumeWinners(@PathVariable int year) {
+    	return topicService.findWinnersByCategory(COSTUME, year);
+    }
+    
 	// returns all movies within input year
     @RequestMapping("year/{year}")
     public List<Movie> getMoviesByYear(@PathVariable int year) {
