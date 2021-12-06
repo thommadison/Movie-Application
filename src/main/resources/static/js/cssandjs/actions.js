52c9fb3994aece49bf661d6985a4c2ead90c16e7
dysco72 <jeffreyscp087@gmail.com>
2021-12-06 00:43:27 -0800

    Refactoring

--- src/main/resources/static/js/cssandjs/actions.js
+++ src/main/resources/static/js/cssandjs/actions.js
@@ -13,7 +13,7 @@ let searchYear = document.getElementById('year');
 let searchFromYear = document.getElementById('fromYear');
 let searchToYear = document.getElementById('toYear');
 let searchCategory;
-let flag = 0;
+let useRangeFlag = 0;
 
 // Advanced search category select function
 function getCategorySelect() {
@@ -26,18 +26,18 @@ document.getElementById('range').onclick = function() {
     let disabledWinner = document.getElementById("winner").disabled;
     // Disable year inputbox
     if (disabledYear) {
-        flag = 0;
+        useRangeFlag = 0;
         document.getElementById("year").disabled = false;
     } else {
-        flag = 1;
+        useRangeFlag = 1;
         document.getElementById("year").disabled = true;
     }
     // Disable winner checkbox
     if (disabledWinner) {
-        flag = 0;
+        useRangeFlag = 0;
         document.getElementById("winner").disabled = false;
     } else {
-        flag = 1;
+        useRangeFlag = 1;
         document.getElementById("winner").disabled = true;
     }
 }
@@ -46,14 +46,24 @@ document.getElementById('range').onclick = function() {
 function submitted(event) {
     let searchTitleUrl;
     let win;
+    let validFlag = 0;
     event.preventDefault();
-    if (SEARCHINPUT.value == '')
-        alert("Input cannot be empty");
-    else if (SEARCHINPUT.value.length < 3)
-        alert("Incorrect input. Search name characters cannot be less than 3.")
-    else {
+    validFlag = searchYearCheck();
+    if (validFlag == 0) {
         searchTitleUrl = DOMAIN + SITE + SEARCH + TITLE + SEARCHINPUT.value;
         win = openTab();
+    }
+
+    function searchYearCheck() {
+        let validFlag = 0;
+        if (SEARCHINPUT.value == '') {
+            alert("Input cannot be empty");
+            validFlag = 1;
+        } else if (SEARCHINPUT.value.length < 3) {
+            alert("Incorrect input. Search name characters cannot be less than 3.");
+            validFlag = 1;
+        }
+        return validFlag;
     }
 
     function openTab() {
@@ -67,38 +77,65 @@ function submitted1(event) {
     let searchCateYearUrl;
     let searchRangeCateUrl;
     let win;
+    let validFlag;
     event.preventDefault();
-    if (flag == 0) {
-        if (searchYear.value == '')
-            alert("Incorrect input. Input year cannot be empty.");
-        else if (isNaN(searchYear.value))
-            alert("Incorrect input. Input year is not a number.");
-        else if (searchYear.value < 1980 || searchYear.value > 2020)
-            alert("Incorrect input. Input year should be between 1980-2020.");
-        else if (searchCategory == undefined)
-            alert("Please select category");
-        else if (document.querySelector('#winner:checked') !== null) {
-            searchWinnerCateUrl = DOMAIN + SITE + WINNER + CATEGORY + searchCategory + "/" + YEAR + searchYear.value;
-            win = openTab(searchWinnerCateUrl);
-        } else {
-            searchCateYearUrl = DOMAIN + SITE + CATEGORY + searchCategory + "/" + YEAR + searchYear.value;
-            win = openTab(searchCateYearUrl);
+    if (useRangeFlag == 0) {
+        let winnerCheckbox = document.querySelector('#winner:checked');
+        validFlag = advancedSearchCheck();
+        if (validFlag == 0) {
+            if (winnerCheckbox !== null) {
+                searchWinnerCateUrl = DOMAIN + SITE + WINNER + CATEGORY + searchCategory + "/" + YEAR + searchYear.value;
+                win = openTab(searchWinnerCateUrl);
+            } else {
+                searchCateYearUrl = DOMAIN + SITE + CATEGORY + searchCategory + "/" + YEAR + searchYear.value;
+                win = openTab(searchCateYearUrl);
+            }
         }
     } else {
-        if (searchFromYear.value == '' || searchToYear.value == '')
-            alert("Incorrect input. Input year cannot be empty.");
-        else if (isNaN(searchFromYear.value) || isNaN(searchToYear.value))
-            alert("Incorrect input. Input year is not a number.");
-        else if (searchFromYear.value < 1980 || searchFromYear.value > 2020 || searchToYear.value < 1980 || searchToYear.value > 2020) 
-            alert("Incorrect input. Input year should be between 1980-2020.");
-        else if (searchFromYear.value >= searchToYear.value)
-            alert("Incorrect input. Input from year should be less than to year.");
-        else if (searchCategory == undefined)
-            alert("Please select category");
-        else {
+        validFlag = advancedSearchRangeYearCheck();
+        if (validFlag == 0) {
             searchRangeCateUrl = DOMAIN + SITE + CATEGORY + searchCategory + "/start/" + searchFromYear.value + "/end/" + searchToYear.value;
             win = openTab(searchRangeCateUrl);
         }
+    }
+
+    function advancedSearchCheck() {
+        let validFlag = 0;
+        if (searchYear.value == '') {
+            alert("Incorrect input. Input year cannot be empty.");
+            validFlag = 1;
+        } else if (isNaN(searchYear.value)) {
+            alert("Incorrect input. Input year is not a number.");
+            validFlag = 1;
+        } else if (searchYear.value < 1980 || searchYear.value > 2020) {
+            alert("Incorrect input. Input year should be between 1980-2020.");
+            validFlag = 1;
+        } else if (searchCategory == undefined) {
+            alert("Please select category");
+            validFlag = 1;
+        }
+        return validFlag;
+    }
+    
+    function advancedSearchRangeYearCheck() {
+        let validFlag = 0;
+        if (searchFromYear.value == '' || searchToYear.value == '') {
+            alert("Incorrect input. Input year cannot be empty.");
+            validFlag = 1;
+        } else if (isNaN(searchFromYear.value) || isNaN(searchToYear.value)) {
+            alert("Incorrect input. Input year is not a number.");
+            validFlag = 1;
+        } else if (searchFromYear.value < 1980 || searchFromYear.value > 2020 || searchToYear.value < 1980 || searchToYear.value > 2020) {
+            alert("Incorrect input. Input year should be between 1980-2020.");
+            validFlag = 1;
+        } else if (searchFromYear.value >= searchToYear.value) {
+            alert("Incorrect input. Input from year should be less than to year.");
+            validFlag = 1;
+        } else if (searchCategory == undefined) {
+            alert("Please select category");
+            validFlag = 1;
+        }
+        return validFlag;
     }
 
     function openTab(searchUrl) {
