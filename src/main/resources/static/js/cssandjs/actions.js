506ba1b580d32a0701fa1696a4711439586ed409
dysco72 <jeffreyscp087@gmail.com>
2021-12-05 21:08:06 -0800

    Refactoring

--- src/main/resources/static/js/cssandjs/actions.js
+++ src/main/resources/static/js/cssandjs/actions.js
@@ -1,7 +1,7 @@
 // constants
-const F = document.getElementById('form');
-const Q = document.getElementById('query');
-const F1 = document.getElementById('form1');
+const SEARCHFORM = document.getElementById('form');
+const SEARCHINPUT = document.getElementById('query');
+const ADVANCEDSEARCHFORM = document.getElementById('form1');
 const DOMAIN = 'http://localhost:8080/';
 const SITE = 'awardmovies/';
 const SEARCH = 'search-movie?';
@@ -9,29 +9,31 @@ const TITLE = 'Title=';
 const WINNER = 'winning/';
 const CATEGORY = 'category/';
 const YEAR = 'year/';
-let y = document.getElementById('year');
-let fy = document.getElementById('fromYear');
-let ty = document.getElementById('toYear');
-let category1;
+let searchYear = document.getElementById('year');
+let searchFromYear = document.getElementById('fromYear');
+let searchToYear = document.getElementById('toYear');
+let searchCategory;
 let flag = 0;
 
 // Advanced search category select function
-function categorySelect() {
-    category1 = document.getElementById('category').value;
+function getCategorySelect() {
+    searchCategory = document.getElementById('category').value;
 }
 
 // Advanced search use range button function
 document.getElementById('range').onclick = function() {
-    let disabled = document.getElementById("year").disabled;
-    let disabled1 = document.getElementById("winner").disabled;
-    if (disabled) {
+    let disabledYear = document.getElementById("year").disabled;
+    let disabledWinner = document.getElementById("winner").disabled;
+    // Disable year inputbox
+    if (disabledYear) {
         flag = 0;
         document.getElementById("year").disabled = false;
     } else {
         flag = 1;
         document.getElementById("year").disabled = true;
     }
-    if (disabled1) {
+    // Disable winner checkbox
+    if (disabledWinner) {
         flag = 0;
         document.getElementById("winner").disabled = false;
     } else {
@@ -45,12 +47,12 @@ function submitted(event) {
     let searchTitleUrl;
     let win;
     event.preventDefault();
-    if (Q.value == '')
+    if (SEARCHINPUT.value == '')
         alert("Input cannot be empty");
-    else if (Q.value.length < 3)
+    else if (SEARCHINPUT.value.length < 3)
         alert("Incorrect input. Search name characters cannot be less than 3.")
     else {
-        searchTitleUrl = DOMAIN + SITE + SEARCH + TITLE + Q.value;
+        searchTitleUrl = DOMAIN + SITE + SEARCH + TITLE + SEARCHINPUT.value;
         win = openTab();
     }
 
@@ -67,35 +69,34 @@ function submitted1(event) {
     let win;
     event.preventDefault();
     if (flag == 0) {
-        if (y.value == '')
+        if (searchYear.value == '')
             alert("Incorrect input. Input year cannot be empty.");
-        else if (isNaN(y.value))
+        else if (isNaN(searchYear.value))
             alert("Incorrect input. Input year is not a number.");
-        else if (y.value < 1980 || y.value > 2020)
+        else if (searchYear.value < 1980 || searchYear.value > 2020)
             alert("Incorrect input. Input year should be between 1980-2020.");
-        else if (category1 == undefined)
+        else if (searchCategory == undefined)
             alert("Please select category");
         else if (document.querySelector('#winner:checked') !== null) {
-            searchWinnerCateUrl = DOMAIN + SITE + WINNER + CATEGORY + category1 + "/" + YEAR + y.value;
+            searchWinnerCateUrl = DOMAIN + SITE + WINNER + CATEGORY + searchCategory + "/" + YEAR + searchYear.value;
             win = openTab(searchWinnerCateUrl);
         } else {
-            // searchCateYearUrl = DOMAIN + SITE + category1 + "/" + y.value;
-            searchCateYearUrl = DOMAIN + SITE + CATEGORY + category1 + "/" + YEAR + y.value;
+            searchCateYearUrl = DOMAIN + SITE + CATEGORY + searchCategory + "/" + YEAR + searchYear.value;
             win = openTab(searchCateYearUrl);
         }
     } else {
-        if (fy.value == '' || ty.value == '')
+        if (searchFromYear.value == '' || searchToYear.value == '')
             alert("Incorrect input. Input year cannot be empty.");
-        else if (isNaN(fy.value) || isNaN(ty.value))
+        else if (isNaN(searchFromYear.value) || isNaN(searchToYear.value))
             alert("Incorrect input. Input year is not a number.");
-        else if (fy.value < 1980 || fy.value > 2020 || ty.value < 1980 || ty.value > 2020) 
+        else if (searchFromYear.value < 1980 || searchFromYear.value > 2020 || searchToYear.value < 1980 || searchToYear.value > 2020) 
             alert("Incorrect input. Input year should be between 1980-2020.");
-        else if (fy.value >= ty.value)
+        else if (searchFromYear.value >= searchToYear.value)
             alert("Incorrect input. Input from year should be less than to year.");
-        else if (category1 == undefined)
+        else if (searchCategory == undefined)
             alert("Please select category");
         else {
-            searchRangeCateUrl = DOMAIN + SITE + CATEGORY + category1 + "/start/" + fy.value + "/end/" + ty.value;
+            searchRangeCateUrl = DOMAIN + SITE + CATEGORY + searchCategory + "/start/" + searchFromYear.value + "/end/" + searchToYear.value;
             win = openTab(searchRangeCateUrl);
         }
     }
@@ -105,5 +106,5 @@ function submitted1(event) {
     }
 }
 // Event Listener
-F.addEventListener('submit', submitted);
-F1.addEventListener('submit', submitted1);
+SEARCHFORM.addEventListener('submit', submitted);
+ADVANCEDSEARCHFORM.addEventListener('submit', submitted1);
